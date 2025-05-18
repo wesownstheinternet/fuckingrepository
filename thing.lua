c0bc00c0c0bcd38ec48402e9848c29db268ec6d8
wesownstheinternet <158778941+wesownstheinternet@users.noreply.github.com>
2025-05-17 20:35:39 -0500

    Update thing.lua

--- thing.lua
+++ thing.lua
@@ -1,2 +1,3 @@
-print ("hello world")
+print ("Welcome to my Web-Site! Not much here until I actually learn to code (or chatgpt get good at it). Until then, Im stuck printing stuff with LUA.")
+print ("Wish me luck trying to figure out anything. Have a day!"
 end
