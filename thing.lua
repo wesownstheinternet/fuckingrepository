ced747a8c9130043433c2795d17b25250b0a477e
wesownstheinternet <158778941+wesownstheinternet@users.noreply.github.com>
2025-05-17 20:40:31 -0500

    Delete thing.lua

--- thing.lua
+++
@@ -1,3 +0,0 @@
-print ("Welcome to my Web-Site! Not much here until I actually learn to code (or chatgpt get good at it). Until then, Im stuck printing stuff with LUA.")
-print ("Wish me luck trying to figure out anything. Have a day!"
-end
